8d06e3966d1c02cd76f666b6c26139e26a51180f
Jorge Cadena Argote <aztrock@users.noreply.github.com>
2015-10-16 19:22:08 -0500

    Redireccionaminto cuando el host no se encuentra en el ALLOWED_HOSTS

--- django_host_extend/middleware.py
+++ django_host_extend/middleware.py
@@ -1,17 +1,30 @@
 # vim: set expandtab ts=4 sw=4:
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
+from django.http import HttpResponseRedirect
 from django_host_extend.models import HostExtendModel
 
 class HostExtendMiddleware(object):
+
     def process_request(self, request):
-        host = list(HostExtendModel.objects.filter(is_active=True).values_list('host', flat=True))
-        settings.ALLOWED_HOSTS.extend(set(host).difference(settings.ALLOWED_HOSTS))
-    
+        host = cache.get("host_list")
+        if not host:
+            host = list(
+                HostExtendModel.objects.filter(is_active=True).values_list('host', flat=True)
+            )
+            cache.set("host_list", host)
+            settings.ALLOWED_HOSTS = ["127.0.0.1"]
+        #settings.ALLOWED_HOSTS.extend(set(host).difference(settings.ALLOWED_HOSTS))
+        if request.META['HTTP_HOST'].split(':')[0] not in host:
+            return HttpResponseRedirect(settings.HE_REDIRECT)
+        settings.ALLOWED_HOSTS.extend(host)
+
     def process_view(self, request, view_func, view_args, view_kwargs):
         http_host = request.META['HTTP_HOST'].split(':')
-        try:
-            qs = HostExtendModel.objects.get(host=http_host[0])
-        except ObjectDoesNotExist:
-            return None
-        print (qs.image.url)
+        host = cache.get("HostExtend:" + http_host[0])
+        if not host:
+            try:
+                HostExtendModel.objects.get(host=http_host[0], is_active=True)
+                cache.set("HostExtend:" + http_host[0], True)
+            except ObjectDoesNotExist:
+                return HttpResponseRedirect(settings.HE_REDIRECT)
